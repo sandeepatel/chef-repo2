5429163f96f725aad4d53e479f5c21b65760f267
sandeepatel <sandeep.chetty@gmail.com>
2017-04-29 17:48:49 +0000

    apache

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'apache' do
+	package_name 'httpd'
+	action :install
+end
+
+service 'httpd' do
+	action [:enable,:start]
+end
---
+++ cookbooks/apache/recipes/websites.rb
@@ -0,0 +1,6 @@
+file '/var/www/html/index.html' do
+	content 'hello'
+	mode '0755'
+	owner 'root'
+	group 'apache'
+end
